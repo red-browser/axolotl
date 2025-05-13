b4a2f19947ca54d3f0d0b1ab39955658782abd8e
rein <manavrj.07@gmail.com>
2025-05-13 17:55:13 +0000

    css parsing for hover and others basic (#5)
    * added: css parsing for hover and others basic
    
    * added: css parsing for hover and others basic

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use html::dom::Node;
 use html::Parser;
 use std::env;
 use std::fs;
+use std::path::Path;
 
 fn pretty_print(node: &Node, indent: usize) {
     let spaces = " ".repeat(indent);
@@ -67,8 +68,10 @@ fn parse_inline_styles(node: &Node, stylesheet: &mut css::rules::Stylesheet) {
         NodeType::Element(elem) if elem.tag_name == "style" => {
             if let Some(text_node) = node.children.first() {
                 if let NodeType::Text(css_text) = &text_node.node_type {
-                    let mut css_parser = css::parser::CssParser::new(css_text.clone());
-                    stylesheet.rules.extend(css_parser.parse_stylesheet().rules);
+                    let mut css_parser = css::parser::CssParser::new(css_text.as_str());
+                    if let Ok(parsed) = css_parser.parse_stylesheet() {
+                        stylesheet.rules.extend(parsed.rules);
+                    }
                 }
             }
         }
@@ -82,37 +85,55 @@ fn parse_inline_styles(node: &Node, stylesheet: &mut css::rules::Stylesheet) {
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-
-    let html = if args.len() > 1 {
-        fs::read_to_string(&args[1])?
-    } else {
-        fs::read_to_string("tests/html/test.html")?
-    };
-
-    let mut html_parser = Parser::new(html);
-    let dom = html_parser.parse();
-
-    let css = if args.len() > 1 {
-        fs::read_to_string(&args[1])?
-    } else {
-        fs::read_to_string("tests/css/test.css")?
-    };
-
-    let mut css_parser = css::parser::CssParser::new(css.to_string());
-    let stylesheet = css_parser.parse_stylesheet();
-
-    println!("Parsed CSS Rules:");
-    for rule in stylesheet.rules {
-        if let css::rules::Rule::Style(style_rule) = rule {
-            println!("Selectors: {:?}", style_rule.selectors);
-            for decl in style_rule.declarations {
-                println!("  {}: {:?}", decl.name, decl.value);
-            }
-        }
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file>", args[0]);
+        std::process::exit(1);
     }
 
-    for child in &dom.children {
-        pretty_print(child, 0);
+    let file_path = &args[1];
+    let content = fs::read_to_string(file_path)?;
+
+    let path = Path::new(file_path);
+    match path.extension().and_then(|ext| ext.to_str()) {
+        Some("html") => {
+            let mut html_parser = Parser::new(content);
+            let dom = html_parser.parse();
+            pretty_print(&dom, 0);
+        }
+        Some("css") => {
+            let mut css_parser = css::parser::CssParser::new(&content);
+            match css_parser.parse_stylesheet() {
+                Ok(stylesheet) => {
+                    println!("Parsed CSS Rules:");
+                    for rule in stylesheet.rules {
+                        match rule {
+                            css::rules::Rule::Style(style_rule) => {
+                                println!("Selectors: {:?}", style_rule.selectors);
+                                for decl in style_rule.declarations {
+                                    println!("  {}: {:?}", decl.name, decl.value);
+                                }
+                            }
+                            css::rules::Rule::Media { query, rules } => {
+                                println!("@media {} {{", query);
+                                println!("}}");
+                            }
+                            css::rules::Rule::Keyframes { name, frames } => {
+                                println!("@keyframes {} {{", name);
+                                println!("}}");
+                            }
+                        }
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error parsing CSS: {}", e);
+                    std::process::exit(1);
+                }
+            }
+        }
+        _ => {
+            eprintln!("Unsupported file type. Please use .html or .css");
+            std::process::exit(1);
+        }
     }
 
     Ok(())
